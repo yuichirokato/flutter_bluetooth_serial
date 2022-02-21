97630e5ce995090ab0ae5af1f7b9fc80b69a013a
y.takahashi <ikmy0709@gmail.com>
2022-02-21 22:45:20 +0900

    Feature: minimum support for iOS

--- ios/Classes/SwiftFlutterBluetoothSerialPlugin.swift
+++ ios/Classes/SwiftFlutterBluetoothSerialPlugin.swift
@@ -37,11 +37,11 @@ enum FlutterMethodName: String {
     case write
 }
 
-public class SwiftFlutterBluetoothSerialPlugin: NSObject, FlutterPlugin {
-    private static let stateEventChannelStreamHandler = StateEventChannelStreamHandler()
-    private static let discveryEventChannelStreamHandler = DiscoveryEventChannelStreamHandler()
+private let stateEventChannelStreamHandler = StateEventChannelStreamHandler()
+private let discveryEventChannelStreamHandler = DiscoveryEventChannelStreamHandler()
 
-    private var centralManager: CBCentralManager?
+public class SwiftFlutterBluetoothSerialPlugin: NSObject, FlutterPlugin {
+    private var bluetoothManager: BluetoothManager?
     private var isInitialized = false
 
     public static func register(with registrar: FlutterPluginRegistrar) {
@@ -59,9 +59,8 @@ public class SwiftFlutterBluetoothSerialPlugin: NSObject, FlutterPlugin {
 
     public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
         print("FlutterBluetoothSerial: handle called: ", call.method)
-        if !isInitialized {
-            centralManager = CBCentralManager(delegate: self, queue: nil)
-            isInitialized = true
+        if bluetoothManager == nil {
+            bluetoothManager = BluetoothManager()
         }
 
         guard let method = FlutterMethodName(rawValue: call.method) else {
@@ -71,25 +70,18 @@ public class SwiftFlutterBluetoothSerialPlugin: NSObject, FlutterPlugin {
 
         switch method {
         case .isAvailable:
-            result(true)
+            result(bluetoothManager?.isAvailable ?? false)
 
-        case .isOn:
-            result(true)
+        case .isOn, .isEnabled:
+            result(bluetoothManager?.isOn ?? false)
 
-        case .isEnabled:
-            result(true)
-
-        case .openSettings:
-            print("\(method.rawValue) called.")
-            result("iOS " + UIDevice.current.systemVersion)
-
-        case .requestEnable:
-            print("\(method.rawValue) called.")
-            result("iOS " + UIDevice.current.systemVersion)
-
-        case .requestDisable:
-            print("\(method.rawValue) called.")
-            result("iOS " + UIDevice.current.systemVersion)
+        case .openSettings, .requestEnable, .requestDisable:
+            if let url = URL(string: UIApplication.openSettingsURLString) {
+                UIApplication.shared.open(url)
+                result(true)
+            } else {
+                result(false)
+            }
 
         case .ensurePermissions:
             print("\(method.rawValue) called.")
@@ -97,23 +89,26 @@ public class SwiftFlutterBluetoothSerialPlugin: NSObject, FlutterPlugin {
 
         case .getState:
             print("\(method.rawValue) called.")
-            result("iOS " + UIDevice.current.systemVersion)
+            print("state int: \(bluetoothManager?.mangerStateInt)")
+            result(bluetoothManager?.mangerStateInt)
 
         case .getAddress:
+            // It can't get mac address of device in iOS App.
+            // So return uuid instead.
             print("\(method.rawValue) called.")
-            result("iOS " + UIDevice.current.systemVersion)
+            result(UIDevice.current.identifierForVendor?.uuidString ?? "")
 
         case .getName:
             print("\(method.rawValue) called.")
-            result("iOS " + UIDevice.current.systemVersion)
+            result(UIDevice.current.name)
 
         case .setName:
             print("\(method.rawValue) called.")
-            result("iOS " + UIDevice.current.systemVersion)
+            result(FlutterError(code: "", message: "It can't modify device name from program in iOS", details: nil))
 
         case .getDeviceBondState:
             print("\(method.rawValue) called.")
-            result("iOS " + UIDevice.current.systemVersion)
+            result(bluetoothManager?.deviceBondState(at: call.arguments as? String ?? ""))
 
         case .removeDeviceBond:
             print("\(method.rawValue) called.")
@@ -133,19 +128,21 @@ public class SwiftFlutterBluetoothSerialPlugin: NSObject, FlutterPlugin {
 
         case .getBondedDevices:
             print("\(method.rawValue) called.")
-            result("iOS " + UIDevice.current.systemVersion)
+            result(bluetoothManager?.bondedDevice())
 
         case .isDiscovering:
             print("\(method.rawValue) called.")
-            result("iOS " + UIDevice.current.systemVersion)
+            result(bluetoothManager?.isDiscovering)
 
         case .startDiscovery:
             print("\(method.rawValue) called.")
-            result("iOS " + UIDevice.current.systemVersion)
+            bluetoothManager?.startDiscovery()
+            result(nil)
 
         case .cancelDiscovery:
             print("\(method.rawValue) called.")
-            result("iOS " + UIDevice.current.systemVersion)
+            bluetoothManager?.cancelDiscovery()
+            result(nil)
 
         case .isDiscoverable:
             print("\(method.rawValue) called.")
@@ -166,34 +163,114 @@ public class SwiftFlutterBluetoothSerialPlugin: NSObject, FlutterPlugin {
     }
 }
 
+class BluetoothManager: NSObject {
+    var isAvailable: Bool {
+        centralManager?.state != .unsupported && centralManager?.state != .unknown
+    }
+    var isOn: Bool {
+        centralManager?.state == .poweredOn
+    }
+    var mangerStateInt: Int {
+        centralManager.map { self.convertToStateNumber(from: $0.state) } ?? -1
+    }
+    var isDiscovering: Bool {
+        centralManager?.isScanning ?? false
+    }
 
-extension SwiftFlutterBluetoothSerialPlugin: CBCentralManagerDelegate {
-    public func centralManagerDidUpdateState(_ central: CBCentralManager) {
-        switch central.state {
-        case .poweredOn:
-            NSLog("poweredOn")
-            //startScan()
-        case .poweredOff:
-            // Alert user to turn on Bluetooth
-            NSLog("poweredOff")
-        case .resetting:
-            // Wait for next state update and consider logging interruption of Bluetooth service
-            NSLog("resetting")
-        case .unauthorized:
-            // Alert user to enable Bluetooth permission in app Settings
-            NSLog("unauthorized")
-        case .unsupported:
-            // Alert user their device does not support Bluetooth and app will not work as expected
-            NSLog("unsupported")
+    private var centralManager: CBCentralManager?
+    private var scannedPeripherals: [(peripheral: CBPeripheral, rssi: Int)] = []
+
+    override init() {
+        super.init()
+
+        centralManager = CBCentralManager(delegate: self, queue: nil)
+        print("manager state: \(String(describing: centralManager?.state))")
+    }
+
+    func connect(with argument: Any?) throws {
+//        let data = FlutterStandardDataType(rawValue: argument)
+    }
+
+    func startDiscovery() {
+        centralManager?.scanForPeripherals(withServices: nil)
+    }
+
+    func cancelDiscovery() {
+        centralManager?.stopScan()
+    }
+
+    func bondedDevice() -> [[String: Any]] {
+        scannedPeripherals.filter { $0.peripheral.state == .connected }.map { self.convertToDictionary(from: $0.peripheral, rssi: $0.rssi) }
+    }
+
+    func deviceBondState(at identifier: String) -> Int {
+        scannedPeripherals
+            .first { $0.peripheral.identifier.uuidString == identifier }
+            .map { self.convertToConnectedNumber(from: $0.peripheral.state) } ?? 0
+    }
+}
+
+extension BluetoothManager: CBCentralManagerDelegate {
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        print("new state: \(central.state)")
+        stateEventChannelStreamHandler.sink(stateInteger: convertToStateNumber(from: central.state))
+    }
+
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+        scannedPeripherals.append((peripheral: peripheral, rssi: RSSI.intValue))
+        discveryEventChannelStreamHandler.sink(argument: convertToDictionary(from: peripheral, rssi: RSSI.intValue))
+    }
+}
+
+private extension BluetoothManager {
+    func convertToStateNumber(from state: CBManagerState) -> Int {
+        switch state {
         case .unknown:
-            // Wait for next state update
-            NSLog("unknown")
+            return state.rawValue
+        case .unsupported, .unauthorized, .resetting: // TODO: set "right" number
+            return 0
+        case .poweredOff:
+            return 10
+        case .poweredOn:
+            return 12
+        }
+    }
+
+    func convertToDictionary(from peripheral: CBPeripheral, rssi: Int) -> [String: Any] {
+        let name = peripheral.name ?? ""
+        let address = peripheral.identifier.uuidString
+        let type = 0
+        let isConnected = false // TODO: It need calculate
+        let bondState = convertToConnectedNumber(from: peripheral.state)
+
+        return [
+            "name": name,
+            "address": address,
+            "type": type,
+            "isConnected": isConnected,
+            "bondState": bondState,
+            "rssi": rssi
+        ]
+    }
+
+    func convertToConnectedNumber(from state: CBPeripheralState) -> Int {
+        switch state {
+        case .disconnected, .disconnecting:
+            return 10
+        case .connecting:
+            return 11
+        case .connected:
+            return 12
         }
     }
 }
 
 class StateEventChannelStreamHandler: NSObject, FlutterStreamHandler {
     private(set) var stateSink: FlutterEventSink?
+
+    public func sink(stateInteger: Int) {
+        stateSink?(stateInteger)
+    }
 
     public func onListen(withArguments arguments: Any?, eventSink events: @escaping FlutterEventSink) -> FlutterError? {
         stateSink = events
@@ -211,6 +288,10 @@ class StateEventChannelStreamHandler: NSObject, FlutterStreamHandler {
 class DiscoveryEventChannelStreamHandler: NSObject, FlutterStreamHandler {
     private(set) var discoverySink: FlutterEventSink?
 
+    public func sink(argument: Any?) {
+        discoverySink?(argument)
+    }
+
     public func onListen(withArguments arguments: Any?, eventSink events: @escaping FlutterEventSink) -> FlutterError? {
         discoverySink = events
 
@@ -223,3 +304,4 @@ class DiscoveryEventChannelStreamHandler: NSObject, FlutterStreamHandler {
         return nil
     }
 }
+
